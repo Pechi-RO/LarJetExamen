bf07a51c67ff26d53e464f20673e1408678cdc6f
FranciscoRuizOrtega <71519403+FranciscoRuizOrtega@users.noreply.github.com>
2022-06-15 14:31:05 +0200

    miercoles mañana

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -9,11 +9,11 @@ class Comic extends Model
 {
     use HasFactory;
     protected $fillable=['nombre','privado','image','editorial','category_id','user_id'];
-    function users(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
-    function categories(){
+    public function category(){
         return $this->belongsTo(Category::class);
     }
-    
+
 }
